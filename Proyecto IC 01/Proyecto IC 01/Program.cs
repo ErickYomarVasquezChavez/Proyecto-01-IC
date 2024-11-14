40842a4827575ab79b4e03f197dd615d0b9a225c
Erick Yomar Vásquez CHávez <erickvasquezchavez@gmail.com>
2024-11-14 12:24:30 -0500

    Tercera actualización del Proyecto

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es Erick");
 Console.WriteLine("Mi numero de celular es :914946139");
+Console.WriteLine("Mi correo electronico es :erickvasquezchavez@gmail.com");
